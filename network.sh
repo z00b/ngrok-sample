5102a8f83f214aed1bf8d7201e3e6e928cd54eaa
Robert Zuber <rob@circleci.com>
2022-05-09 19:36:41 -0700

    sleep fixes everything

--- network.sh
+++ network.sh
@@ -4,6 +4,7 @@ if [[ "0" == ${CIRCLE_NODE_INDEX} ]]
 then
 	echo controller
 	python -m http.server 8080 &
+	sleep 5
 	curl --verbose --max-time 5 http://localhost:8080
 	ngrok http --subdomain=${CIRCLE_WORKFLOW_ID} 8080
 else
