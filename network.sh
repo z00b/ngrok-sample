b95fa7856398146ca50c4256d83900a76d7c40bf
Robert Zuber <rob@circleci.com>
2022-05-09 19:34:13 -0700

    so much debugging

--- network.sh
+++ network.sh
@@ -4,10 +4,11 @@ if [[ "0" == ${CIRCLE_NODE_INDEX} ]]
 then
 	echo controller
 	python -m http.server 8080 &
-	curl --verbose --max-time=5 http://localhost:8080
+	curl --verbose --max-time 5 http://localhost:8080
 	ngrok http --subdomain=${CIRCLE_WORKFLOW_ID} 8080
 else
 	echo agent
 	sleep 30
-	curl --verbose --max-time=5 http://${CIRCLE_WORKFLOW_ID}.ngrok.io:8080/
+	echo "curl'ing http://${CIRCLE_WORKFLOW_ID}.ngrok.io:8080/"
+	curl --verbose --max-time 5 http://${CIRCLE_WORKFLOW_ID}.ngrok.io:8080/
 fi
